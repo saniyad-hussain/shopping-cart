701b60c8d41f0cb50fe515af7a10920ba50387d5
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-07-22 21:54:36 -0400

    plus and minus btn working through a common function

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+//Increment of cart
 const plusBtn = document.getElementById("plusBtn");
+let cartValue = document.getElementById("cartValueplus").value;
+let cartValueNumber = parseInt(cartValue);
+let iphonePrice = 1219;
 plusBtn.addEventListener("click", function(){
-    let cartValuePlus = document.getElementById("cartValueplus").value;
-    let cartValuePlusNumber = parseInt(cartValuePlus);
-    cartValuePlusNumber = cartValuePlusNumber +1;
-    document.getElementById("cartValueplus").value = cartValuePlusNumber;
 
-    let iphonePriceSpot = document.getElementById("iphonePrice").innerText;
-    let iphonePrice = 1219;
-    let totalPrice = cartValuePlusNumber * iphonePrice;
-    document.getElementById("iphonePrice").innerText = totalPrice;
+    cartValueNumber = cartValueNumber +1;
+    document.getElementById("cartValueplus").value = cartValueNumber;
+
+    // let iphonePriceSpot = document.getElementById("iphonePrice").innerText;
+    priceCalculation(cartValueNumber, iphonePrice);
 });
+
+//Discrement of cart
+
+const minusBtn = document.getElementById("minusBtn");
+minusBtn.addEventListener("click", function(){
+    if(cartValueNumber > 1){
+        cartValueNumber = cartValueNumber -1;
+        document.getElementById("cartValueplus").value = cartValueNumber;
+        priceCalculation(cartValueNumber, iphonePrice);
+    }
+    else{
+        alert("Cart Value cant be zero");
+    }
+});
+
+
+function priceCalculation(cartValueNumber, iphonePrice){
+    let totalPrice = cartValueNumber * iphonePrice;
+    document.getElementById("iphonePrice").innerText = totalPrice;
+}
